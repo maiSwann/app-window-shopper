a35a08320ae8a62558a4617230e2b7efb227e3bf
mailys <mailys.perez@epitech.eu>
2020-06-24 10:12:42 +0200

    initial xcode project

---
+++ app-window-shopper/app-window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-window-shopper
+//
+//  Created by Maïlys Perez on 24/06/2020.
+//  Copyright © 2020 Maïlys Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
